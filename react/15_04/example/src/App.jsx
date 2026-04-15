e05999279754e54ea0388f98c892a4f479ecd630
yahalom98 <twixy100@gmail.com>
2026-04-15 13:30:41 +0300

    updated

--- react/15_04/example/src/App.jsx
+++ react/15_04/example/src/App.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
-import Ecommerce from './components/Ecommerce'
+import React from "react";
+import Ecommerce from "./components/Ecommerce";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import Home from "./components/Home";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <div>
-      <Ecommerce/>
-    </div>
-  )
+
+    <BrowserRouter>
+        <div className="nav-container">
+          <ul>
+            <li>
+                <Link to={'/'}>Home</Link>
+            </li>
+            <li>
+                <Link to={'/Ecommerce'}>Ecommerce</Link>
+            </li>
+            <li>
+                <Link to={'/Contact'}>Contact</Link>
+            </li>
+          </ul>
+        </div>
+
+      <Routes>
+        <Route path="/" element={<Home />}/>
+         <Route path="/Ecommerce" element={<Ecommerce />} />
+          <Route path="/Contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
