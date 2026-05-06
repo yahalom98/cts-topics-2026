40ab650028fb7aaaf93d88efdaeee10c4634cda1
yahalom98 <twixy100@gmail.com>
2026-05-06 10:09:49 +0300

    as

--- node/06_05/app.js
+++ node/06_05/app.js
@@ -1,11 +1,32 @@
-const mongodb = require('mongodb')
-const MongoClient = mongodb.MongoClient
-const connectionURL = 'mongodb://127.0.0.1:27017'
-const databaseName = 'task-manager'
-MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client)=> {
-if (error) {
-return console.log('Unable to connect to database!')
-}
-const db = client.db(databaseName)
-// Start to interact with the database
-})
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+const connectionURL = "mongodb://localhost:27017/test";
+const databaseName = "test";
+MongoClient.connect(
+  connectionURL,
+  { useNewUrlParser: true },
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to database!");
+    }
+    const db = client.db(databaseName);
+    db.collection("kooli").insertMany(
+      [
+        {
+          description: "Clean the house",
+          completed: true,
+        },
+        {
+          description: "Renew inspection",
+          completed: false,
+        },
+      ],
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to insert tasks!");
+        }
+        console.log(result.ops);
+      },
+    );
+  },
+);
